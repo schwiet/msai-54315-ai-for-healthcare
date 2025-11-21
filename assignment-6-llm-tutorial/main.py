ab161aa1720262a3e9360e1a5daa9a6834892b92
schwiet <seth.schwiet@pm.me>
2025-11-20 21:46:57 -0800

    a little work on importing dataset and setting up OpenAI client

--- assignment-6-llm-tutorial/main.py
+++ assignment-6-llm-tutorial/main.py
@@ -7,4 +7,31 @@ if openai_key and openai_key.strip():
 else:
     print("OPENAI_KEY is empty or not set")
 
+import pandas as pd
+patients = pd.read_csv("10k_synthea_covid19_csv/patients.csv")
+encounters = pd.read_csv("10k_synthea_covid19_csv/encounters.csv")
+conditions = pd.read_csv("10k_synthea_covid19_csv/conditions.csv")
+patients.info()
+patients.head()
+encounters.info()
+encounters.head()
+conditions.info()
+conditions.head()
 
+conditions["DESCRIPTION"].value_counts()
+
+parse_dates = ["START","STOP","DATE","BIRTHDATE","DEATHDATE","ONSET","RECORDED_DATE"]
+
+# normalize date columns
+for df in (patients, encounters, conditions):
+    for c in [c for c in parse_dates if c in df.columns]:
+        df[c] = pd.to_datetime(df[c], errors="coerce")
+
+
+from openai import OpenAI
+client = OpenAI(api_key=openai_key)
+
+response = client.responses.create(
+    model="gpt-5-nano",
+    input="Write a short bedtime story about a unicorn."
+)
