bcd8fe386f146291c60dead8e1f1e7464d37be81
schwiet <seth.schwiet@pm.me>
2025-11-20 21:05:30 -0800

    added synthetic covid19 dataset directory to .gitignore and updated key method to add api key to env

--- assignment-6-llm-tutorial/main.py
+++ assignment-6-llm-tutorial/main.py
@@ -1,7 +1,10 @@
-import os
+import getpass
 
-openai_key = os.getenv('OPENAI_KEY')
+# Get the OpenAI API key and confirm it is set
+openai_key = getpass.getpass("MY_API_KEY: ")
 if openai_key and openai_key.strip():
     print("OPENAI_KEY is non-empty")
 else:
     print("OPENAI_KEY is empty or not set")
+
+
